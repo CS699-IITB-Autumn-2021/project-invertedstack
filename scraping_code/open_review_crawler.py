99ac7b4fc45d90b360055e84d170a63b9eb2e1f6
Ashita Saxena <as.saxena.as@gmail.com>
2021-10-12 21:20:30 +0530

    Added code to get categories from iclr 2021 conf

--- scraping_code/open_review_crawler.py
+++ scraping_code/open_review_crawler.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import json
 
 class openReview:
     
@@ -95,6 +96,47 @@ class openReview:
 
         return iclr_confs
 
+    def get_iclr_conf_link(self,url):
+        driver = webdriver.Chrome(executable_path=self.chrome_path, options=self.chrome_options)
+        # Run the Webdriver, save page an quit browser
+        driver.get(url)
+
+        ele_available = False
+        while not ele_available:
+            elements = driver.find_elements_by_css_selector("div#notes a")
+            if(len(elements)>0):
+                ele_available = True
+        iclr_conf_link=""
+        for element in elements:
+            if("conference" in element.get_attribute("innerHTML").lower()):
+                iclr_conf_link =  element.get_attribute("href")
+            
+
+        driver.quit()
+
+        return iclr_conf_link
+
+    def get_categories_from_iclr_conf(self,url):
+        driver = webdriver.Chrome(executable_path=self.chrome_path, options=self.chrome_options)
+        # Run the Webdriver, save page an quit browser
+        driver.get(url)
+
+
+        ele_available = False
+        while not ele_available:
+            elements = driver.find_elements_by_css_selector("ul.nav-tabs a")
+            if(len(elements)>0):
+                ele_available = True
+        iclr_conf_cats = {}
+        for element in elements:
+            iclr_conf_link = element.get_attribute("href")
+            iclr_conf_name = element.get_attribute("innerHTML")
+            if "your" not in iclr_conf_name.lower():
+                iclr_conf_cats[iclr_conf_name.strip()] = iclr_conf_link
+
+        driver.quit()
+
+        return iclr_conf_cats
 
 if __name__ == "__main__":
     
@@ -106,8 +148,25 @@ if __name__ == "__main__":
     print(OR.get_open_venues(home_url))
     all_venues = OR.get_all_venues(home_url)
     
+    print(all_venues)
+    
     all_iclr = OR.get_list_of_iclr_conf(all_venues['ICLR'])
     print(all_iclr)
 
-    print(all_venues)
+
+    #get conf name 
+    for name in all_iclr:
+        if '2021' in name:
+            iclr_2021_conf = all_iclr[name]
+    
+
+    #get iclr conference link        
+    iclr_conf_link = OR.get_iclr_conf_link(iclr_2021_conf)
+    print("ICLR CONF LINK",iclr_conf_link)
+
+    #get all categories from a iclr conference
+    categories = OR.get_categories_from_iclr_conf(iclr_conf_link)
+
+    with open("iclr_2021_categories.json","w") as f:
+        json.dump(categories,f,indent=6)
 
