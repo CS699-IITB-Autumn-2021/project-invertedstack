b412ab928ee1dc5a91344fd36d8a6352ad36edad
Tejpalsingh Siledar <tjsiledar@gmail.com>
2021-10-13 15:05:06 +0530

    Added comments to the initial implementation.

--- api_code/app.py
+++ api_code/app.py
@@ -6,11 +6,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'This is my first API call!'
+    """
+    Demo function to check the api calls.
+
+    Returns : string
+    """
+    return 'This is the first API call!'
 
 
 @app.route("/get_parameters")
 def get_parameters():
+    """
+    Will fetch the required parameters from the json database files.
+
+    Returns : json output
+    """
     category = request.args["category"]
     category_map = {"oral_presentations":"Review_Papers/iclr_2021_oral_presentations.json", 
     "poster_presentations":"Review_Papers/iclr_2021_poster_presentations.json",
@@ -27,6 +37,11 @@ def get_parameters():
 
 @app.route("/get_categories")
 def get_categories():
+    """
+    Will get the categories from the json database files.
+
+    Returns : json output
+    """
     f = open("Review_Papers/iclr_2021_categories.json")
     data = json.load(f)
     return jsonify(data)
