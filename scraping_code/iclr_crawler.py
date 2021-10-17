2447f50894a6b0ad938ed378c0c0a97bb0a2c493
Ashita Saxena <as.saxena.as@gmail.com>
2021-10-17 13:38:46 +0530

    making code modular

--- scraping_code/iclr_crawler.py
+++ scraping_code/iclr_crawler.py
@@ -1,28 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import json,time
+import json,time,os
 import pprint
 from tqdm import tqdm
+import Config
 
-class ICLR:
+config = Config.Config()
+
+
+class PageCrawler:
     def __init__(self):
-        '''
-        Create Options instance and add initial arguments
-        set chrome options and add chrome path
-        '''
         self.chrome_options = Options()
         self.chrome_options.add_argument("--headless") # Hides the browser window
         # Reference the local Chromedriver instance
-        self.chrome_path = r'chromedriver'
+        self.chrome_path = config.chrome_driver_path
+      
 
     def get_all_venues(self,url):
-        '''
-        Gets all venues that are available in home page
-        Arguments:
-        url - home page url
-        Return Value:
-        Returns a dictionary containing all venues names and their urls
-        '''
         driver = webdriver.Chrome(executable_path=self.chrome_path, options=self.chrome_options)
         # Run the Webdriver, save page an quit browser
         driver.get(url)
@@ -44,13 +38,6 @@ class ICLR:
 
     
     def get_list_of_iclr_conf(self,url):
-        '''
-        gets the lists of all iclr conferences 
-        Argument:
-        url - url of ICLR open review page
-        Return Value:
-        returns a dictionary of all ICLR conferences and their url
-        '''
 
         driver = webdriver.Chrome(executable_path=self.chrome_path, options=self.chrome_options)
         # Run the Webdriver, save page an quit browser
@@ -74,13 +61,6 @@ class ICLR:
 
 
     def get_iclr_conf_link(self,url):
-        '''
-        Gets the url of ICLR conference (not workshop) from the url
-        Arguments:
-        url - the url of particual year's ICLR 
-        Return Value:
-        returns the link of conference of ICLR for a particular year
-        '''
         driver = webdriver.Chrome(executable_path=self.chrome_path, options=self.chrome_options)
         # Run the Webdriver, save page an quit browser
         driver.get(url)
@@ -101,13 +81,6 @@ class ICLR:
         return iclr_conf_link
 
     def get_categories_from_iclr_conf(self,url):
-        '''
-        Every ICLR conference has 4-5 categories. This function gets all the categories of a particual ICLR conference for a particular year.
-        Argument:
-        url - the url of a particular year's ICLR conference
-        Return Value:
-        A dictionary containing the category name and their corresponding link
-        '''
         driver = webdriver.Chrome(executable_path=self.chrome_path, options=self.chrome_options)
         # Run the Webdriver, save page an quit browser
         driver.get(url)
@@ -129,15 +102,7 @@ class ICLR:
 
         return iclr_conf_cats
 
-    def get_conf_papers_from_category(self, category_url, year, category, limit=500):
-        '''
-        This functions scrapes all the papers from the ICLR conference page of a particular year and a particular category.
-        Arguments:
-        category_url - The url of ICLR for a particular year and a particular category
-        year - the year of the ICLR conference
-        category - the category of the ICLR conference
-        limit - Number of papers to be extracted
-        '''
+    def get_conf_papers_from_category(self, category_url, year, category, output_path, limit=config.limit):
         driver = webdriver.Chrome(executable_path=self.chrome_path, options=self.chrome_options)
         # Run the Webdriver, save page an quit browser
         driver.get(category_url)
@@ -155,7 +120,8 @@ class ICLR:
                 ele_available = True
                 
         list_of_all_papers = []
-        
+        if not limit:
+            limit = len(li_of_all_papers)
         for element in tqdm(li_of_all_papers[:limit], "creating data"):
             details_of_curr_paper = {}
             #get data-id
@@ -182,11 +148,9 @@ class ICLR:
                 details_of_curr_paper[key[:-1].lower().replace(' ','-')]=value
             list_of_all_papers.append(details_of_curr_paper)
 
-        #creates the path of output json file
-        outputfile = "iclr_"+year+"_"+category.lower().replace("/"," ").replace(" ","_")+".json"
+
+        outputfile = output_path+"/iclr_"+year+"_"+category.lower().replace("/"," ").replace(" ","_")+".json"
         print("creating file : ",outputfile)
-        
-        #Create json file and dump all data for a particular year ICLR conference for a particular category
         with open(outputfile,"w") as f:
             json.dump(list_of_all_papers,f,indent=6)
         print("File created!!")
@@ -195,47 +159,12 @@ class ICLR:
         return list_of_all_papers
         
 
-    def save_json_of_all_iclr(self,url,year,categories):
-        '''
-        Gets the papers for all categories of a particular year's ICLR conference
-        Arguments:
-        url - url of ICLR conference of a particular category
-        year - year of the ICLR conference
-        categories - a dictionary containing all the categories of that particular ICLR conference
-        '''
+    def save_json_of_all_iclr(self,url,year,categories,output_dir):
+        output_path = output_dir + "iclr_" + year
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
         for cat in categories:
-            self.get_conf_papers_from_category(url,year,cat)
+            self.get_conf_papers_from_category(categories[cat],year,cat,output_path)
         
 
 
-if __name__ == "__main__":
-    #url of the home page
-    home_url = "https://openreview.net/"
-
-
-    OR = ICLR()
-    
-    #get all venues
-    all_venues = OR.get_all_venues(home_url)
-
-    #get list of all iclr entries in open review
-    iclr_conf = OR.get_list_of_iclr_conf(all_venues['ICLR'])
-
-    #saving all the paper details for 2020 2019 2018 in json format
-    for name in iclr_conf:
-        #getting papers for 2021,2020,2019,2018
-        if any(year in name for year in ['2021','2020','2019','2018']):
-            categories = {}
-            conf_link=""
-            with open(name.lower().replace(" ","_")+"_cateogries.json","w") as f:
-                #get ICLR conf link from the name of ICLR conf
-                conf_link = OR.get_iclr_conf_link(iclr_conf[name])
-                #get categories from the particular ICLR conference 
-                print("getting categories from - ", conf_link)
-                categories = OR.get_categories_from_iclr_conf(conf_link)
-                #create a json file containing all categories of ICLR conference
-                json.dump(categories,f,indent=6)
-            print("calling save_json_of_all_iclr ", name.strip()[-4:], " ", conf_link)
-            #getting all papers from all ICLR conferences for all categories
-            OR.save_json_of_all_iclr(conf_link, name.strip()[-4:], categories)
-        
