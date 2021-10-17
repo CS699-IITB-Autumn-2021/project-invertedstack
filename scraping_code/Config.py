c196ef8a8ce7de37b9438948e5094e43d07c47df
Ashita Saxena <as.saxena.as@gmail.com>
2021-10-17 14:11:25 +0530

    updated config

--- scraping_code/Config.py
+++ scraping_code/Config.py
@@ -2,7 +2,7 @@ class Config:
     #specify the path of directory where scraped json files will be stored
     data_dir = '../data/'
     #specify the path where chromedriver is placed
-    chrome_driver_path = r'../chromedriver'
+    chrome_driver_path = r'./chromedriver'
     #specify the home-url which needs to be scraped
     #this version supports only open-review
     home_url =  'https://openreview.net/'
@@ -10,7 +10,7 @@ class Config:
     #this version scrapes all conferences of ICLR venue
     venue_name = 'ICLR'
     #specify the list of years which needs to be scraped
-    years = ['2021']
+    years = ['2021','2020','2019','2018']
     #specify the maximum number of papers that needs to be scraped for every category
     #if all papers need to be scraped then keep limit as None
     limit = 50
