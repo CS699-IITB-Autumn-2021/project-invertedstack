46a19ba2ceab4224e8ff2d2084e8c28b056a14d8
Tejpalsingh Siledar <tjsiledar@gmail.com>
2021-10-25 15:03:51 +0530

    update code. added get_info

--- api_code/app.py
+++ api_code/app.py
@@ -37,18 +37,41 @@ def get_parameters():
     #         output_dict[i["data_id"]]["keywords"] = i["keywords"]
     return jsonify(output_dict)
 
-@app.route("/get_categories")
-def get_categories():
+@app.route("/get_info")
+def get_info():
     """
     Will get the categories from the json database files.
 
     Returns : json output
     """
-    year = request.args["year"]
-    conference = request.args["conference"]
-    f = open(database_folder+conference+"_"+year+"_categories.json")
-    data = json.load(f)
-    return jsonify(data)
+    years = ["2018","2019","2020","2021"]
+    conferences = ["iclr"]
+    global_conf_list=[]
+    for conference in conferences:
+        conference_dict = {}
+        data = []
+        for year in years:
+            temp_dict={}
+            categories={}
+            f = open(database_folder+conference+"_"+year+"_categories.json")
+            file_data = json.load(f)
+            for i in list(file_data.keys()):
+                categories[i.lower().replace(" ","_")]=i
+            temp_dict["year"] = year
+            temp_dict["categories"] = categories
+            data.append(temp_dict)
+        conference_dict["name"] = conference
+        conference_dict["readable_name"] = conference.upper()
+        conference_dict["data"]=data
+        global_conf_list.append(conference_dict)
+    return jsonify(global_conf_list)
+
+
+    # year = request.args["year"]
+    # conference = request.args["conference"]
+    # f = open(database_folder+conference+"_"+year+"_categories.json")
+    # data = json.load(f)
+    # return jsonify(data)
 
 # @app.route("/get_years")
 # def get_years():
