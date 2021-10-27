ae0d0749c3dc57baf86b9d6a480d7f4eb58c160f
Ashita Saxena <as.saxena.as@gmail.com>
2021-10-27 13:44:43 +0530

    Updated acc to ppep8 conventions

--- scraping/scraping_code/Config.py
+++ scraping/scraping_code/Config.py
@@ -25,20 +25,20 @@ class Config:
     '''
     This class contains variables that can be configured as per need. 
     '''
-    #specify the path of directory where scraped json files will be stored
+    # specify the path of directory where scraped json files will be stored
     data_dir = '../data/'
-    #specify the path where chromedriver is placed
+    # specify the path where chromedriver is placed
     chrome_driver_path = r'./chromedriver'
-    #specify the home-url which needs to be scraped
-    #this version supports only open-review
+    # specify the home-url which needs to be scraped
+    # this version supports only open-review
     home_url =  'https://openreview.net/'
-    #specify the name of venue which needs to be scraped
-    #this version scrapes all conferences of ICLR venue
+    # specify the name of venue which needs to be scraped
+    # this version scrapes all conferences of ICLR venue
     venue_name = 'ICLR'
-    #specify the list of years which needs to be scraped
+    # specify the list of years which needs to be scraped
     years = ['2021']
-    #specify the maximum number of papers that needs to be scraped for every category
-    #if all papers need to be scraped then keep limit as None
+    # specify the maximum number of papers that needs to be scraped for every category
+    # if all papers need to be scraped then keep limit as None
     limit = 10
     
     
