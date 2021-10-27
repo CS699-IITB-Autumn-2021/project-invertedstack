eb542de7f089db9b0cc3f3e32a476eaf3d7fc0be
Ashita Saxena <as.saxena.as@gmail.com>
2021-10-27 13:20:37 +0530

    Update crawl_forum_comments.py

--- scraping/scraping_code/crawl_forum_comments.py
+++ scraping/scraping_code/crawl_forum_comments.py
@@ -5,6 +5,10 @@
 This file is imported in the main.py file and functions are called
 for a particular forum_id and a given output path.
 
+Contains code to scrape the comments from a forum of a particular paper.
+The code creates a json file per forum containing all forum comments
+and their replies in a proper nested structure.
+
 **Author** - Ashita Saxena (21Q050009)
 
 """
